a2e9005672b94cae3a3329584faf69f3f81d8f5e
양지한 <han@iacryl.com>
2018-07-17 18:53:05 +0900

    repactoring code

--- nodejs/nodejs-sample/app.js
+++ nodejs/nodejs-sample/app.js
@@ -2,22 +2,13 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var mysql = require('mysql')
 var main = require('./router/main')
+var email = require('./router/email')
 
-var connection = mysql.createConnection ({
-    host : 'localhost',
-    prot : 3306,
-    user : 'root',
-    password : 'yang123',
-    database: 'test'
-})
-
-connection.connect();
-
-// No 'Access-Control-Allow-Origin'
 // npm install cors --save
 var cors = require('cors');
+// No 'Access-Control-Allow-Origin'
+app.use(cors({origin: '*'}))
 
 app.listen(3000, function(){
     console.log("start test");
@@ -35,46 +26,17 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 // view engine은 ejs야 라고 알려줌 
 app.set('view engine', 'ejs')
+// 미리 html을 만들어두고 응답 ( ex) ejs 을 통해 ) 
+// npm install ejs --save
 
+// 각 url호출시 부를 거 
 app.use('/main', main)
-
-// No 'Access-Control-Allow-Origin'
-app.use(cors({origin: '*'}))
-
+app.use('/email', email)
 
 app.get('/', function(req,res) {
     //res.send("<h1>test</h1>")
     res.sendFile(__dirname + "/public/main.html")
 })
-
-
-app.post('/email_post', function(req,res) {
-    console.log(req.body.email)
-//    res.send("<h1>welcome" + req.body.email + "</h1>")
-    res.render('email.ejs',{'email' : req.body.email})
-})
-
-app.post('/ajax_send_email', function(req, res){
-    var email = req.body.email;
-    var responseData = {}; 
-
-    var query = connection.query('select name from user where email="'+ email + '"', function(err, rows ){
-        if(err) throw err;
-        if(rows[0]) {
-            // console.log(rows[0].name);
-            responseData.result = "ok";
-            responseData.name = rows[0].name;
-        } else { 
-            responseData.result = "none";
-            responseData.name = "";
-        }
-        res.json(responseData)
-    })
-
-});
-
-// 미리 html을 만들어두고 응답 ( ex) ejs 을 통해 ) 
-// npm install ejs --save
 
 //for(var i=0; i < 1000 ;i++) {
 //    console.log('ggg');
